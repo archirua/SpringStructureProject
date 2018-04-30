c1874d0feff79242d82e7b8053a68dfb883176bf
ISMSadmin <ISMSadmin@ISMSadmin-PC>
2018-04-30 13:19:25 +0900

    Aop 설정 반영

--- SpringStructureProject/src/main/java/com/archirua/common/aop/CommonRestAop.java
+++ SpringStructureProject/src/main/java/com/archirua/common/aop/CommonRestAop.java
@@ -1,17 +1,23 @@
 package com.archirua.common.aop;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.support.BindingAwareModelMap;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @Aspect
 public class CommonRestAop {
 	
 	@Before("execution(* com.archirua.common.rest.*.*(..))")
-	public void doSomthing( JoinPoint joinpoint ) {
+	public void doSomthingRest( JoinPoint joinpoint ) {
 		System.out.println("RestAop");
-	}	
+	}
 	
 }
