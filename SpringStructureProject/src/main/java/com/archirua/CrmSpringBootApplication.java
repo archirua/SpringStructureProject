6031c5a802a198ea8956855b727a1b923d1809ab
achirua in company <moni.yongkou@gmail.com>
2018-05-15 17:59:57 +0900

    사무실 Upload

--- SpringStructureProject/src/main/java/com/archirua/CrmSpringBootApplication.java
+++ SpringStructureProject/src/main/java/com/archirua/CrmSpringBootApplication.java
@@ -27,13 +27,13 @@ public class CrmSpringBootApplication {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+/*
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
-
+*/
         };
     }
 }
